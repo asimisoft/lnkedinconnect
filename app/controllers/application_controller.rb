9de8ee370d4370d240a7b8eca6df80655de674a8
AsimiSoft <amazingrails@gmail.com>
2017-08-14 16:35:32 +0545

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def  after_sign_out_path_for(resource_or_scope)
+  def  after_sign_out_path_for(resource_or_scope) 
     homes_path
   end
 
